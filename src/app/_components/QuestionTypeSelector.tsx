1ec59e2859c0d24781322a391af7a04534e424c8
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-08-01 20:43:23 +0800

    Updated QuestionTypeSelector: sends selected type to parent component's QuizType state

--- src/app/_components/QuestionTypeSelector.tsx
+++ src/app/_components/QuestionTypeSelector.tsx
@@ -1,25 +1,26 @@
 "use client"
 import { useState } from "react";
+import { QuizType } from "../page";
 
-type InputOption = 'Multiple Choice' | 'True/False' | 'Identification';
+interface QuestionTypeSelector {
+    quizType: QuizType,
+    handleTypeChange: (quizType: QuizType) => void
+}
 
-export default function QuestionTypeSelector() {
-
-    const [selectedOption, setSelectedOption] = useState<InputOption>('Multiple Choice');
-
-    const options: InputOption[] = ['Multiple Choice', 'True/False', 'Identification'];
+export default function QuizTypeSelector({ quizType, handleTypeChange }: QuestionTypeSelector) {
+    const options: QuizType[] = ['Multiple Choice', 'True/False', 'Identification'];
 
     return (
         <>
             {/* The Segmented Control Container */}
             <div className="flex w-full max-w-sm rounded-full border border-slate-300 overflow-hidden bg-white font-inter">
                 {options.map((option, index) => {
-                    const isActive = selectedOption === option;
+                    const isActive = quizType === option;
 
                     return (
                         <button
                             key={option}
-                            onClick={() => setSelectedOption(option)}
+                            onClick={() => handleTypeChange(option)}
                             className={`
                 flex-1 py-2 px-4 text-sm font-semibold transition-colors
                 ${index !== options.length - 1 ? 'border-r border-slate-300' : ''}
