6f8e26fdece3063b031feda5d1f274c9ed9b4c74
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-08-02 20:53:44 +0800

    Removed NavBar on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Orbitron, Inter } from "next/font/google"
-import NavBar from "./_components/NavBar";
 
 const orbitron = Orbitron({
   subsets: ["latin"],
@@ -27,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${orbitron.variable} ${inter.variable}`}>
-      <body>      <NavBar />{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
