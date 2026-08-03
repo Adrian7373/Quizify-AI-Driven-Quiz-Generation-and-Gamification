eace3edd993dffa9adbc01e700338b09024a6e1b
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-08-03 22:45:03 +0800

    Supabase browser client

---
+++ src/utils/supabase/client.ts
@@ -0,0 +1,9 @@
+import { createBrowserClient } from '@supabase/ssr'
+
+export function createClient() {
+    // createBrowserClient automatically handles reading the session from the browser's cookies
+    return createBrowserClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+}
