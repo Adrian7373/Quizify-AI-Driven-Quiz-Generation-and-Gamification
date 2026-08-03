dda31aa74dcd610fc2bb501997edca2f35c53a41
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-08-03 22:36:34 +0800

    Added user Name and aiCredits to the NavBar

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -1,14 +1,24 @@
-import { CircleUserRound } from "lucide-react";
+import { CirclePoundSterling, CircleUserRound } from "lucide-react";
 import Logo from "./Logo";
+import { AppUser } from "../page";
 
+interface NavBarProps {
+    user: AppUser | null
+}
 
-export default function NavBar() {
+export default function NavBar({ user }: NavBarProps) {
     return (
         <div className="fixed w-full bg-darker flex py-4 items-center justify-between px-2">
             <Logo />
-            <div className="flex gap-4">
-                <button className="text-white">Contact Us</button>
-                <CircleUserRound className="text-white" />
+            <div className="flex flex-col gap-1">
+                <div className="flex items-center gap-2 text-white">
+                    <p>{user?.name}</p>
+                    <CircleUserRound className="text-white" />
+                </div>
+                <div className="flex items-center gap-2 text-white justify-end">
+                    <p>{user?.aiCredits}</p>
+                    <CirclePoundSterling />
+                </div>
             </div>
         </div>
     )
